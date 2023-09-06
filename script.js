bb350cbdcd61576596024e532af0a4c3103c94b3
kanhaiya2y <93575895+kanhaiya2y@users.noreply.github.com>
2023-09-06 13:13:26 +0530

    project

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
 
 
+
 //  var scrollIntetrval = setInterval(function()
 //  {
 //     window .scrollBy(0,50);
@@ -34,20 +35,70 @@
 //  skills  .addEventListener('click',function()
 
 
-var navmenuanchortag= document.querySelector('.nav-menu a');
-console.log(navmenuanchortag);
- for( i-0; i<navmenuanchortag.clientHeight;i++){
-  navmenuanchortag.addEventListener('click',function(event){
-event.preventDefault();
+// var navmenuanchortag= document.querySelector('.nav-menu a');
+// console.log(navmenuanchortag);
+//  for( i-0; i<navmenuanchortag.clientHeight;i++){
+//   navmenuanchortag.addEventListener('click',function(event){
+// event.preventDefault();
  
 
-var targetsectionID=this.textcontent.trim().tolowersection();
- var targetsection=document.getElementById(targetsectionID);
+// var targetsectionID=this.textcontent.trim().tolowersection();
+//  var targetsection=document.getElementById(targetsectionID);
 
- conbsole.log(targetsection); }
-  var  targetsectioncoordinate= targetsection.getBoundingClientRect();
-  );
- }
+//  conbsole.log(targetsection); }
+//   var  targetsectioncoordinate= targetsection.getBoundingClientRect();
+//   );
+//  }
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
 
 
  
